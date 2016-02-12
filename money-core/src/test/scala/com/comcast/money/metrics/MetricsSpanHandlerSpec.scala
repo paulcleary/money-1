3fad9462a57235c626891b140baca6d6bfa82d28
Cleary, Paul <Paul_Cleary@cable.comcast.com>
2016-02-12 09:20:07 -0500

    Moving over metrics
    Moved the metric registry factory over.
    
    Created new MetricSpanHandler

---
+++ money-core/src/test/scala/com/comcast/money/metrics/MetricsSpanHandlerSpec.scala
@@ -0,0 +1,10 @@
+package com.comcast.money.metrics
+
+import org.scalatest.{Matchers, WordSpec}
+
+class MetricsSpanHandlerSpec extends WordSpec with Matchers {
+
+  "MetricsSpanHandler" should {
+
+  }
+}
